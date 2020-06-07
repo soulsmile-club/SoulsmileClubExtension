0f4968a18a9c28cb2c7a2f1685a02ead187af580
Sneha Rampalli <svrampalli@gmail.com>
2020-06-07 16:43:34 -0400

    initial boilerplate code for browser extension

---
+++ background.js
@@ -0,0 +1,14 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: 'blue'}, function() {
+        console.log('The color is blue.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'developer.chrome.com'},
+            })], actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
