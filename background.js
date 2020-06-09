073c910e9429cb46bc014e07c204d4e16003cffa
Sneha Advani <sneha.d.advani@gmail.com>
2020-06-08 23:36:49 -0400

    Created dialog box for permissions and injected affiliate tag for all Amazon URLs

--- background.js
+++ background.js
@@ -4,10 +4,13 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: 'blue'}, function() {
         console.log('The color is blue.');
     });
+    chrome.storage.sync.set({isEarning: false}, function() {
+        console.log('This user is not earning.');
+    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+            pageUrl: {hostEquals: 'www.amazon.com'},
             })], actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
