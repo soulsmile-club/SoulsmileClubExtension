4ec3a8d603b6447e2d648cce123ed80c8534343e
Sneha Rampalli <svrampalli@gmail.com>
2020-06-09 17:02:21 -0400

    old code

--- background.js
+++ background.js
@@ -7,8 +7,9 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+            pageUrl: {hostEquals: 'www.amazon.com'},
             })], actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
 });
+
