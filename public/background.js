9918c0f1bc186543d9bb129f4134b35aa0fe2691
Sneha Rampalli <svrampalli@gmail.com>
2020-07-20 22:36:28 -0400

    Changed how-to-link to correspond with new site

--- public/background.js
+++ public/background.js
@@ -2,7 +2,7 @@
 
 chrome.runtime.onInstalled.addListener(function() {
 	// creates new tab with extension instructions when browser extension is first installed
-    chrome.tabs.create({url: 'https://www.soulsmile.club/blog/how-to-use-extension'}, 
+    chrome.tabs.create({url: 'https://www.soulsmile.club/how-to-use'}, 
     function () {
         console.log('Created tab to demonstrate how to use extension');
     });
