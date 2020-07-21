da2447de5655191bea15565c8c571f4a146b8c59
Sneha Rampalli <svrampalli@gmail.com>
2020-07-21 15:54:49 -0400

    added email (login/signup) functionality

--- src/Account.js
+++ src/Account.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+// import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
@@ -9,7 +13,25 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-import { Button } from '@material-ui/core';
+const useStyles = makeStyles({
+  root: {
+		backgroundColor: 'white',
+		padding: '5px 5px',
+		margin: '5px 0px',
+		width: '75%',
+		color: '#eda1aa'
+	},
+	focused: {
+		color: '#eda1aa'
+	},
+  label: {
+		fontFamily: 'Montserrat !important',
+  },
+  button: {
+    fontFamily: 'Montserrat !important',
+    fontSize: '14px !important',
+  }
+});
 
 var firebaseConfig = {
 	    apiKey: process.env.REACT_APP_API_KEY,
@@ -30,26 +52,65 @@ console.log('Initialized firebase app');
  * TODO: Add login and account functionality
 */
 function Account() {
+	const classes = useStyles();
+	const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+	const [name, setName] = React.useState('');
+	const [username, setUsername] = React.useState('');
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const [photoURL, setPhotoURL] = React.useState('');
+	const [isLogin, setIsLogin] = React.useState(true);
 
-		const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-		const [name, setName] = React.useState('');
-		const [email, setEmail] = React.useState('');
-		const [photoURL, setPhotoURL] = React.useState('');
-
-    useEffect(() => {
-			firebase.auth().onAuthStateChanged(function(user) {
-				if (user) {
-					console.log(user);
-					setName(user.displayName);
-					setEmail(user.email);
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				setName(user.displayName);
+				setUsername(user.email);
+				if (user.photoURL) {
 					setPhotoURL(user.photoURL + "?type=large");
-					setIsLoggedIn(true);
 				} else {
-					console.log('no user found');
-					setIsLoggedIn(false);
+					setPhotoURL(null);
 				}
-			});
-    });
+				setIsLoggedIn(true);
+			} else {
+				console.log('no user found');
+				setIsLoggedIn(false);
+			}
+		});
+	});
+
+	function emailLoginPopup () {
+		firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(function (user) {
+			setIsLoggedIn(true);
+		})
+		.catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// ...
+		});
+	}
+
+	async function emailSignupPopup () {
+		await firebase.auth().createUserWithEmailAndPassword(email, password).then(async function(result) {
+			var user = firebase.auth().currentUser;
+			await user.updateProfile({
+				displayName: name
+			}).then(function () {
+				// Hacky way to set the name - TODO: user reload
+				setName(user.displayName);
+				console.log('Email signup with popup');
+			})
+		})
+		.catch(function(error) {
+			console.log(error);
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// ...
+		});
+	}
 
 	function googleLoginPopup () {
 		var provider = new firebase.auth.GoogleAuthProvider();
@@ -80,7 +141,6 @@ function Account() {
 		  var token = result.credential.accessToken;
 		  // The signed-in user info.
 			var user = result.user;
-			console.log(user);
 		  setIsLoggedIn(true);
 		}).catch(function(error) {
 			console.log(error);
@@ -150,56 +210,147 @@ function Account() {
 	//   }
 	// });
 
-		function signOut () {
-			firebase.auth().signOut().then(function() {
-				// Sign-out successful.
-				setIsLoggedIn(false);
-			}).catch(function(error) {
-				// An error happened.
-			});
-		}
+	function loginOrSignup (e) {
+		e.preventDefault();
+		setIsLogin(!isLogin);
+	}
 
-		var logoutButton = (
-			<>
-				<div className="profile">
-					<img id="photo" src={photoURL} alt={name}></img>
-					<h1>{name}</h1>
-				</div>
-				
-				<Button id='logoutButton' 
-						variant='contained' 
-						color='default' 
-						onClick={signOut}>
-						Log Out
-				</Button>
-			</>
-		);
+	function signOut () {
+		firebase.auth().signOut().then(function() {
+			// Sign-out successful.
+			setIsLoggedIn(false);
+		}).catch(function(error) {
+			// An error happened.
+		});
+	}
 
-    var loginButtons = (
-    	<div>
-    	<Button id='googleLoginButton' 
-                variant='contained' 
-                color='default' 
-                onClick={googleLoginPopup}
-            >
-                Login with Google
-        </Button>
-        <Button id='facebookLoginButton' 
-                variant='contained' 
-                color='default' 
-                onClick={facebookLoginPopup}
-            >
-                Login with Facebook
-        </Button>
-        </div>
-    );
+	var logoutButton = (
+		<>
+			<div className="profile">
+				{photoURL ? <img id="photo" src={photoURL} alt={name}></img> : <></>}
+				<h1>{name}</h1>
+			</div>
+			
+			<Button id='logoutButton' 
+					variant='contained' 
+					color='default' 
+					onClick={signOut}>
+					Log Out
+			</Button>
+		</>
+	);
 
-    return (
-    	<>
-	        <div id="soul">Your Account</div>
-            {isLoggedIn ? logoutButton : loginButtons}
-	    </>
-    );
+	var signupButtons = (
+		<div>
+			<Button id='googleLoginButton' 
+							variant='outlined' 
+							color='default'
+							onClick={googleLoginPopup}
+							classes={{root: classes.root, label: classes.label, button: classes.button}}>
+				Sign up with Google
+			</Button>
+			<Button id='facebookLoginButton' 
+							variant='outlined' 
+							color='default'
+							onClick={facebookLoginPopup}
+							classes={{root: classes.root, label: classes.label, button: classes.button}}>
+				Sign up with Facebook
+			</Button>
+			<hr/>
+			<form autoComplete="off">
+				<TextField 
+					id="outlined-secondary"
+					label="Name"
+					type="text"
+					value={name} 
+					onChange={e => setName(e.target.value)}
+					classes={{root: classes.root, label: classes.label, focused: classes.focused}}
+					variant="outlined" 
+					/>
+				<TextField 
+					id="outlined-secondary"
+					label="Email"
+					type="text"
+					value={email} 
+					onChange={e => setEmail(e.target.value)}
+					classes={{root: classes.root, label: classes.label, focused: classes.focused}}
+					variant="outlined" 
+					/>
+				<TextField 
+					id="outlined-secondary"
+					label="Password"
+					type="password"
+					value={password} 
+					classes={{root: classes.root, label: classes.label, focused: classes.focused}}
+					onChange={e => setPassword(e.target.value)}
+					variant="outlined" 
+					/>
+				<Button 
+							variant='outlined' 
+							color='default'
+							onClick={emailSignupPopup}
+							classes={{root: classes.root, label: classes.label, button: classes.button}}>
+				Sign up with Email
+			</Button>
+			</form>
+			<p>Have an account already? Log in <a id="loginOrSignup" onClick={loginOrSignup}>here</a>.</p>
+		</div>
+	);
+
+	var loginButtons = (
+		<div>
+			<Button id='googleLoginButton' 
+							variant='outlined' 
+							color='default'
+							onClick={googleLoginPopup}
+							classes={{root: classes.root, label: classes.label, button: classes.button}}>
+				Login with Google
+			</Button>
+			<Button id='facebookLoginButton' 
+							variant='outlined' 
+							color='default'
+							onClick={facebookLoginPopup}
+							classes={{root: classes.root, label: classes.label, button: classes.button}}>
+				Login with Facebook
+			</Button>
+			<hr/>
+			<form autoComplete="off">
+				<TextField 
+					id="outlined-secondary"
+					label="Email"
+					type="text"
+					value={email} 
+					onChange={e => setEmail(e.target.value)}
+					classes={{root: classes.root, label: classes.label, focused: classes.focused}}
+					variant="outlined" 
+					/>
+				<TextField 
+					id="outlined-secondary"
+					label="Password"
+					type="password"
+					value={password} 
+					classes={{root: classes.root, label: classes.label, focused: classes.focused}}
+					onChange={e => setPassword(e.target.value)}
+					variant="outlined" 
+					/>
+				<Button 
+							variant='outlined' 
+							color='default'
+							onClick={emailLoginPopup}
+							classes={{root: classes.root, label: classes.label, button: classes.button}}>
+				Login with Email
+			</Button>
+			</form>
+			<p>Don't have an account yet? Sign up <a id="loginOrSignup" onClick={loginOrSignup}>here</a>.</p>
+		</div>
+	);
+
+	return (
+		<>
+		<div id="soul">Your Account</div>
+		{isLoggedIn ? logoutButton : (isLogin ? loginButtons : signupButtons) }
+		</>
+	);
 }
 
 export default Account;
