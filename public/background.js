da1868a18dc8b124eb565fba23b2a1cabfbdc005
Sneha Advani <sneha.d.advani@gmail.com>
2020-07-30 00:29:59 -0400

    Fixed bug with how-to page opening on every update

--- public/background.js
+++ public/background.js
@@ -1,9 +1,14 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function(details) {
 	// creates new tab with extension instructions when browser extension is first installed
-    chrome.tabs.create({url: 'https://www.soulsmile.club/how-to-use'}, 
-    function () {
-        console.log('Created tab to demonstrate how to use extension');
-    });
+	if (details.reason == "install") {
+		chrome.tabs.create({url: 'https://www.soulsmile.club/how-to-use'}, 
+	    function () {
+	        console.log('Created tab to demonstrate how to use extension');
+	    });
+	} else if (details.reason == "update") {
+		// can add update notification for user when we make major changes (e.g. adding account/giving history)
+	}
+    
 });
