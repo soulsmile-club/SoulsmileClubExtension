a622683191634bef262921c33f4e0b902a3ea7fb
roda-16 <68616011+roda-16@users.noreply.github.com>
2021-04-18 21:30:19 -0400

    Update background.js

--- public/background.js
+++ public/background.js
@@ -41,29 +41,10 @@ chrome.runtime.onInstalled.addListener(function(details) {
     chrome.runtime.reload();
 }); 
 
-//####### WILL BE EDITING BUT JUST PUTTING IT HERE FOR NOW, WILL DEBUG THESE.
-//this function is an add a message listener to get appDB information 
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-  switch (msg.type) {
-    case 'updateState':
-      appDb.users(msg.opts.id).set({ value: msg.opts.value });
-      response('success');
-      break;
-    default:
-      response('unknown request');
-      break;
-  }
-}); 
 
 
-// SAME WITH THIS ONE, WILL BE EDITING 
-// That's it! All your extension needs to do is load from the Chrome local storage before rendering itself. If it or a content script needs to manipulate the RTD, the following snippet should be invoked:
-// B: COPIED, CHECKING NEEDED
-chrome.runtime.sendMessage({type: 'updateState', opts: request.opts}, (response) => {
-  if(response == 'success') {
-    // last thing didn't work, so try to create a function in here, instead of notifications.js like last time and make it dedicataed to fetching something from account.js 
-  }
-});
+
+
 
 chrome.runtime.requestUpdateCheck(function(status) {
     if (status == "update_available") {
