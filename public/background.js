0856240c9a1c4dc3e48b48ef6145bc9b3bfd80dd
roda-16 <68616011+roda-16@users.noreply.github.com>
2021-04-18 20:57:13 -0400

    added in firebase changes, still edit
    bug in the message listener

--- public/background.js
+++ public/background.js
@@ -1,4 +1,31 @@
-'use strict';
+'use strict'; 
+import firebase from '../src/Firebase.js'; 
+
+
+
+const applicationState = { values: [] };
+
+// writes the changes to Chrome Storage
+function updateState(applicationState) {
+  chrome.storage.local.set({ state: JSON.stringify(applicationState) });
+}
+
+
+// this was taken from the account.js 
+function getSoulsmiles(user) {
+        console.log("get soulsmiles earned");
+        firebase.database().ref('users/' + user.uid).once("value", snapshot => {
+            if (snapshot.exists()) {
+                console.log("get soulsmiles earned: user exists");
+		applicationState.values[soulsmilesInWallet] = snapshot.val().soulsmilesInWallet;
+                updateState(applicationState);
+		console.log(snapshot.val().soulsmilesInWallet);
+            } else {
+                console.log("get soulsmiles earned: user does not exist");
+            }
+        });
+  }
+
 
 chrome.runtime.onInstalled.addListener(function(details) {
 	// creates new tab with extension instructions when browser extension is first installed
@@ -12,6 +39,30 @@ chrome.runtime.onInstalled.addListener(function(details) {
 	}
     console.log("updating to version " + details.version);
     chrome.runtime.reload();
+}); 
+
+//####### WILL BE EDITING BUT JUST PUTTING IT HERE FOR NOW, WILL DEBUG THESE.
+//this function is an add a message listener to get appDB information 
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+  switch (msg.type) {
+    case 'updateState':
+      appDb.users(msg.opts.id).set({ value: msg.opts.value });
+      response('success');
+      break;
+    default:
+      response('unknown request');
+      break;
+  }
+}); 
+
+
+// SAME WITH THIS ONE, WILL BE EDITING 
+// That's it! All your extension needs to do is load from the Chrome local storage before rendering itself. If it or a content script needs to manipulate the RTD, the following snippet should be invoked:
+// B: COPIED, CHECKING NEEDED
+chrome.runtime.sendMessage({type: 'updateState', opts: request.opts}, (response) => {
+  if(response == 'success') {
+    // last thing didn't work, so try to create a function in here, instead of notifications.js like last time and make it dedicataed to fetching something from account.js 
+  }
 });
 
 chrome.runtime.requestUpdateCheck(function(status) {
